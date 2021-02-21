834815754991b48a222296f43fa4fa1d8a377cf5
sosozhuang <sosozhuang@163.com>
2021-02-21 23:32:50 +0800

    Add hash util and test cases

---
+++ util/hash.go
@@ -0,0 +1,30 @@
+package util
+
+func Hash(data []byte, seed uint32) uint32 {
+	const m = uint32(0xc6a4a793)
+	const r = uint32(24)
+	start := 0
+	limit := len(data)
+	h := seed ^ (uint32(limit) * m)
+	var w uint32
+	for ; start+4 <= limit; start += 4 {
+		w = DecodeFixed32(data[start:])
+		h += w
+		h *= m
+		h ^= h >> 16
+	}
+	switch limit - start {
+	case 3:
+		h += uint32(data[2]) << 16
+		fallthrough
+	case 2:
+		h += uint32(data[1]) << 8
+		fallthrough
+	case 1:
+		h += uint32(data[0])
+		h *= m
+		h ^= h >> r
+		break
+	}
+	return h
+}
