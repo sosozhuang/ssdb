88a9ab9854990247db356981d2660836a883bd5a
sosozhuang <sosozhuang@163.com>
2021-02-23 10:21:27 +0800

    Add options

---
+++ options.go
@@ -0,0 +1,65 @@
+package ssdb
+
+type CompressionType uint8
+
+const (
+	NoCompression CompressionType = iota
+	SnappyCompression
+)
+
+type Options struct {
+	Comparator           Comparator
+	CreateIfMissing      bool
+	ErrorIfExists        bool
+	ParanoidChecks       bool
+	Env                  Env
+	WriteBufferSize      uint
+	MaxOpenFiles         int
+	BlockSize            uint
+	BlockRestartInterval int
+	MaxFileSize          uint
+	BlockCache           Cache
+	CompressionType      CompressionType
+	ReuseLogs            bool
+	FilterPolicy         FilterPolicy
+}
+
+func NewOptions() *Options {
+	return &Options{
+		Comparator:           BytewiseComparator,
+		CreateIfMissing:      false,
+		ErrorIfExists:        false,
+		ParanoidChecks:       false,
+		Env:                  DefaultEnv(),
+		WriteBufferSize:      4 * 1024 * 1024,
+		MaxOpenFiles:         1000,
+		BlockSize:            4 * 1024,
+		BlockRestartInterval: 16,
+		MaxFileSize:          2 * 1024 * 1024,
+		CompressionType:      SnappyCompression,
+		ReuseLogs:            false,
+		FilterPolicy:         nil,
+	}
+}
+
+type ReadOptions struct {
+	VerifyChecksums bool
+	FillCache       bool
+	Snapshot        Snapshot
+}
+
+func NewReadOptions() *ReadOptions {
+	return &ReadOptions{
+		VerifyChecksums: false,
+		FillCache:       true,
+		Snapshot:        nil,
+	}
+}
+
+type WriteOptions struct {
+	Sync bool
+}
+
+func NewWriteOptions() *WriteOptions {
+	return &WriteOptions{Sync: false}
+}
