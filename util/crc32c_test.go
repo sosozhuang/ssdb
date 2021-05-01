317598e738812903118e46e3b9bcde7784c388d3
sosozhuang <sosozhuang@163.com>
2021-05-01 22:51:52 +0800

    Impr: refactor test function name

--- util/crc32c_test.go
+++ util/crc32c_test.go
@@ -4,22 +4,22 @@ import "testing"
 
 func TestStandardResults(t *testing.T) {
 	buf := make([]byte, 32)
-	TestEqual(uint32(0x8a9136aa), ChecksumValue(buf), "zero slice", t)
+	AssertEqual(uint32(0x8a9136aa), ChecksumValue(buf), "zero slice", t)
 
 	for i := range buf {
 		buf[i] = 0xff
 	}
-	TestEqual(uint32(0x62a8ab43), ChecksumValue(buf), "0xff slice", t)
+	AssertEqual(uint32(0x62a8ab43), ChecksumValue(buf), "0xff slice", t)
 
 	for i := range buf {
 		buf[i] = byte(i)
 	}
-	TestEqual(uint32(0x46dd794e), ChecksumValue(buf), "incremental slice", t)
+	AssertEqual(uint32(0x46dd794e), ChecksumValue(buf), "incremental slice", t)
 
 	for i := range buf {
 		buf[i] = byte(31 - i)
 	}
-	TestEqual(uint32(0x113fdb5c), ChecksumValue(buf), "decremental slice", t)
+	AssertEqual(uint32(0x113fdb5c), ChecksumValue(buf), "decremental slice", t)
 
 	data := [48]byte{
 		0x01, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -27,13 +27,13 @@ func TestStandardResults(t *testing.T) {
 		0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x18, 0x28, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	}
-	TestEqual(uint32(0xd9963a56), ChecksumValue(data[:]), "48 length slice", t)
+	AssertEqual(uint32(0xd9963a56), ChecksumValue(data[:]), "48 length slice", t)
 }
 
 func TestValues(t *testing.T) {
-	TestNotEqual(ChecksumValue([]byte("a")), ChecksumValue([]byte("foo")), "'a' and 'foo'", t)
+	AssertNotEqual(ChecksumValue([]byte("a")), ChecksumValue([]byte("foo")), "'a' and 'foo'", t)
 }
 
 func TestExtend(t *testing.T) {
-	TestEqual(ChecksumValue([]byte("hello world")), ChecksumExtend(ChecksumValue([]byte("hello ")), []byte("world")), "extend", t)
+	AssertEqual(ChecksumValue([]byte("hello world")), ChecksumExtend(ChecksumValue([]byte("hello ")), []byte("world")), "extend", t)
 }
