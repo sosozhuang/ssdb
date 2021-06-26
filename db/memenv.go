480f4a2b7368df096eadb3dd9149e9f07de34c90
sosozhuang <sosozhuang@163.com>
2021-06-26 12:50:38 +0800

    Add memenv.go and memenv_test.go

---
+++ db/memenv.go
@@ -0,0 +1,341 @@
+package db
+
+import (
+	"io/ioutil"
+	"log"
+	"runtime"
+	"ssdb"
+	"ssdb/util"
+	"strings"
+	"sync"
+)
+
+type envFileState struct {
+	refsMutex   sync.Mutex
+	refs        int
+	blocksMutex sync.Mutex
+	blocks      []byte
+}
+
+func newEnvFileState() *envFileState {
+	return &envFileState{
+		refs:   0,
+		blocks: make([]byte, 0),
+	}
+}
+
+func (s *envFileState) ref() {
+	s.refsMutex.Lock()
+	defer s.refsMutex.Unlock()
+	s.refs++
+}
+
+func (s *envFileState) unref() {
+	doDelete := false
+	s.refsMutex.Lock()
+	s.refs--
+	if s.refs < 0 {
+		panic("refs < 0")
+	}
+	if s.refs <= 0 {
+		doDelete = true
+	}
+	s.refsMutex.Unlock()
+	if doDelete {
+		s.finalize()
+	}
+}
+
+func (s *envFileState) size() int {
+	s.blocksMutex.Lock()
+	defer s.blocksMutex.Unlock()
+	return len(s.blocks)
+}
+
+func (s *envFileState) truncate() {
+	s.blocksMutex.Lock()
+	defer s.blocksMutex.Unlock()
+	s.blocks = make([]byte, 0)
+}
+
+func (s *envFileState) read(offset uint64, b []byte) (result []byte, n int, err error) {
+	s.blocksMutex.Lock()
+	defer s.blocksMutex.Unlock()
+	size := uint64(len(s.blocks))
+	if offset > size {
+		err = util.IOError1("Offset greater than file size.")
+		return
+	}
+	available := size - offset
+	n = len(b)
+	if uint64(n) > available {
+		n = int(available)
+	}
+	if n == 0 {
+		return
+	}
+	copy(b, s.blocks[offset:offset+uint64(n)])
+	result = b[:n]
+	return
+}
+
+func (s *envFileState) append(data []byte) error {
+	s.blocksMutex.Lock()
+	defer s.blocksMutex.Unlock()
+	s.blocks = append(s.blocks, data...)
+	return nil
+}
+
+func (s *envFileState) finalize() {
+	s.truncate()
+}
+
+type sequentialFileImpl struct {
+	file *envFileState
+	pos  uint64
+}
+
+func newSequentialFileImpl(file *envFileState) *sequentialFileImpl {
+	f := &sequentialFileImpl{
+		file: file,
+		pos:  0,
+	}
+	f.file.ref()
+	return f
+}
+
+func (f *sequentialFileImpl) Read(b []byte) (result []byte, n int, err error) {
+	result, n, err = f.file.read(f.pos, b)
+	if err == nil {
+		f.pos += uint64(n)
+	}
+	return
+}
+
+func (f *sequentialFileImpl) Skip(n uint64) error {
+	size := uint64(f.file.size())
+	if f.pos > size {
+		return util.IOError1("pos_ > file_->Size()")
+	}
+	available := size - f.pos
+	if n > available {
+		n = available
+	}
+	f.pos += n
+	return nil
+}
+
+func (f *sequentialFileImpl) Finalize() {
+	f.file.unref()
+}
+
+type randomAccessFileImpl struct {
+	file *envFileState
+}
+
+func newRandomAccessFileImpl(file *envFileState) *randomAccessFileImpl {
+	f := &randomAccessFileImpl{file: file}
+	f.file.ref()
+	return f
+}
+
+func (f *randomAccessFileImpl) Read(b []byte, offset int64) ([]byte, int, error) {
+	return f.file.read(uint64(offset), b)
+}
+
+func (f *randomAccessFileImpl) Finalize() {
+	f.file.unref()
+}
+
+type writableFileImpl struct {
+	file *envFileState
+}
+
+func newWritableFileImpl(file *envFileState) *writableFileImpl {
+	f := &writableFileImpl{file: file}
+	f.file.ref()
+	return f
+}
+
+func (f *writableFileImpl) Append(data []byte) error {
+	return f.file.append(data)
+}
+
+func (f *writableFileImpl) Close() error {
+	return nil
+}
+
+func (f *writableFileImpl) Flush() error {
+	return nil
+}
+
+func (f *writableFileImpl) Sync() error {
+	return nil
+}
+
+func (f *writableFileImpl) Finalize() {
+	f.file.unref()
+}
+
+type fileSystem map[string]*envFileState
+type inMemoryEnv struct {
+	ssdb.EnvWrapper
+	mutex   sync.Mutex
+	fileMap fileSystem
+}
+
+func newInMemoryEnv(baseEnv ssdb.Env) *inMemoryEnv {
+	e := &inMemoryEnv{
+		EnvWrapper: ssdb.EnvWrapper{Env: baseEnv},
+		fileMap:    make(map[string]*envFileState),
+	}
+	runtime.SetFinalizer(e, func(e *inMemoryEnv) {
+		for _, f := range e.fileMap {
+			f.unref()
+		}
+	})
+	return e
+}
+
+func (e *inMemoryEnv) NewSequentialFile(fname string) (ssdb.SequentialFile, error) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	var (
+		f  *envFileState
+		ok bool
+	)
+	if f, ok = e.fileMap[fname]; !ok {
+		return nil, util.IOError2(fname, "File not found")
+	}
+	return newSequentialFileImpl(f), nil
+}
+
+func (e *inMemoryEnv) NewRandomAccessFile(fname string) (ssdb.RandomAccessFile, error) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	var (
+		f  *envFileState
+		ok bool
+	)
+	if f, ok = e.fileMap[fname]; !ok {
+		return nil, util.IOError2(fname, "File not found")
+	}
+	return newRandomAccessFileImpl(f), nil
+}
+
+func (e *inMemoryEnv) NewWritableFile(fname string) (ssdb.WritableFile, error) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	var (
+		file *envFileState
+		ok   bool
+	)
+	if file, ok = e.fileMap[fname]; !ok {
+		file = newEnvFileState()
+		file.ref()
+		e.fileMap[fname] = file
+	} else {
+		file.truncate()
+	}
+	return newWritableFileImpl(file), nil
+}
+
+func (e *inMemoryEnv) NewAppendableFile(fname string) (ssdb.WritableFile, error) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	file := e.fileMap[fname]
+	if file == nil {
+		file = newEnvFileState()
+		file.ref()
+		e.fileMap[fname] = file
+	}
+	return newWritableFileImpl(file), nil
+}
+
+func (e *inMemoryEnv) FileExists(fname string) (ok bool) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	_, ok = e.fileMap[fname]
+	return
+}
+
+func (e *inMemoryEnv) GetChildren(dir string) ([]string, error) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	result := make([]string, 0)
+	for filename := range e.fileMap {
+		if len(filename) >= len(dir)+1 && filename[len(dir)] == '/' && strings.HasPrefix(filename, dir) {
+			result = append(result, filename[len(dir)+1:])
+		}
+	}
+	return result, nil
+}
+
+func (e *inMemoryEnv) deleteFileInternal(fname string) {
+	fs, ok := e.fileMap[fname]
+	if !ok {
+		return
+	}
+	fs.unref()
+	delete(e.fileMap, fname)
+}
+
+func (e *inMemoryEnv) DeleteFile(fname string) error {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	if _, ok := e.fileMap[fname]; !ok {
+		return util.IOError2(fname, "File not found")
+	}
+	e.deleteFileInternal(fname)
+	return nil
+}
+
+func (e *inMemoryEnv) CreateDir(_ string) error {
+	return nil
+}
+
+func (e *inMemoryEnv) DeleteDir(_ string) error {
+	return nil
+}
+
+func (e *inMemoryEnv) GetFileSize(fname string) (int, error) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	fs, ok := e.fileMap[fname]
+	if !ok {
+		return 0, util.IOError2(fname, "File not found")
+	}
+	return fs.size(), nil
+}
+
+func (e *inMemoryEnv) RenameFile(src, target string) error {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	if _, ok := e.fileMap[src]; !ok {
+		return util.IOError2(src, "File not found")
+	}
+	e.deleteFileInternal(target)
+	e.fileMap[target] = e.fileMap[src]
+	delete(e.fileMap, src)
+	return nil
+}
+
+func (e *inMemoryEnv) LockFile(_ string) (ssdb.FileLock, error) {
+	return struct{}{}, nil
+}
+
+func (e *inMemoryEnv) UnlockFile(_ ssdb.FileLock) error {
+	return nil
+}
+
+func (e *inMemoryEnv) GetTestDirectory() (string, error) {
+	return "/test", nil
+}
+
+func (e *inMemoryEnv) NewLogger(_ string) (*log.Logger, error) {
+	return log.New(ioutil.Discard, "", log.LstdFlags), nil
+}
+
+func newMemEnv(baseEnv ssdb.Env) ssdb.Env {
+	return newInMemoryEnv(baseEnv)
+}
---
+++ db/memenv_test.go
@@ -0,0 +1,259 @@
+package db
+
+import (
+	"bytes"
+	"ssdb"
+	"ssdb/util"
+	"testing"
+)
+
+type memEnvTest struct {
+	env ssdb.Env
+}
+
+func newMemEnvTest() *memEnvTest {
+	return &memEnvTest{env: newMemEnv(ssdb.DefaultEnv())}
+}
+
+func TestBasics(t *testing.T) {
+	test := newMemEnvTest()
+	util.AssertNotError(test.env.CreateDir("/dir"), "CreateDir", t)
+
+	util.AssertFalse(test.env.FileExists("/dir/non_existent"), "FileExists", t)
+	_, err := test.env.GetFileSize("/dir/non_existent")
+	util.AssertError(err, "GetFileSize", t)
+	children, err := test.env.GetChildren("/dir")
+	util.AssertNotError(err, "GetChildren", t)
+	util.AssertEqual(0, len(children), "children", t)
+
+	writableFile, err := test.env.NewWritableFile("/dir/f")
+	util.AssertNotError(err, "NewWritableFile", t)
+	fileSize, err := test.env.GetFileSize("/dir/f")
+	util.AssertNotError(err, "GetFileSize", t)
+	util.AssertEqual(0, fileSize, "fileSize", t)
+	writableFile.Finalize()
+
+	util.AssertTrue(test.env.FileExists("/dir/f"), "FileExists", t)
+	fileSize, err = test.env.GetFileSize("/dir/f")
+	util.AssertEqual(0, fileSize, "GetFileSize", t)
+	children, err = test.env.GetChildren("/dir")
+	util.AssertEqual(1, len(children), "children", t)
+	util.AssertEqual("f", children[0], "children", t)
+
+	writableFile, err = test.env.NewWritableFile("/dir/f")
+	util.AssertNotError(err, "NewWritableFile", t)
+	util.AssertNotError(writableFile.Append([]byte("abc")), "Append", t)
+	writableFile.Finalize()
+
+	writableFile, err = test.env.NewAppendableFile("/dir/f")
+	util.AssertNotError(err, "NewAppendableFile", t)
+	fileSize, err = test.env.GetFileSize("/dir/f")
+	util.AssertNotError(err, "GetFileSize", t)
+	util.AssertEqual(3, fileSize, "fileSize", t)
+	util.AssertNotError(writableFile.Append([]byte("hello")), "Append", t)
+	writableFile.Finalize()
+
+	fileSize, err = test.env.GetFileSize("/dir/f")
+	util.AssertNotError(err, "GetFileSize", t)
+	util.AssertEqual(8, fileSize, "fileSize", t)
+
+	util.AssertError(test.env.RenameFile("/dir/non_existent", "/dir/g"), "RenameFile", t)
+	util.AssertNotError(test.env.RenameFile("/dir/f", "/dir/g"), "RenameFile", t)
+	util.AssertFalse(test.env.FileExists("/dir/f"), "FileExists", t)
+	util.AssertTrue(test.env.FileExists("/dir/g"), "FileExists", t)
+	fileSize, err = test.env.GetFileSize("/dir/g")
+	util.AssertNotError(err, "GetFileSize", t)
+	util.AssertEqual(8, fileSize, "fileSize", t)
+
+	seqFile, err := test.env.NewSequentialFile("/dir/non_existent")
+	util.AssertError(err, "NewSequentialFile", t)
+	util.AssertTrue(seqFile == nil, "seqFile", t)
+	randFile, err := test.env.NewRandomAccessFile("/dir/non_existent")
+	util.AssertError(err, "NewRandomAccessFile", t)
+	util.AssertTrue(randFile == nil, "randFile", t)
+
+	util.AssertError(test.env.DeleteFile("/dir/non_existent"), "DeleteFile", t)
+	util.AssertNotError(test.env.DeleteFile("/dir/g"), "DeleteFile", t)
+	util.AssertFalse(test.env.FileExists("/dir/g"), "FileExists", t)
+	children, err = test.env.GetChildren("/dir")
+	util.AssertNotError(err, "GetChildren", t)
+	util.AssertEqual(0, len(children), "children", t)
+	util.AssertNotError(test.env.DeleteDir("/dir"), "DeleteDir", t)
+}
+
+func TestMemEnvReadWrite(t *testing.T) {
+	test := newMemEnvTest()
+
+	util.AssertNotError(test.env.CreateDir("/dir"), "CreateDir", t)
+
+	writableFile, err := test.env.NewWritableFile("/dir/f")
+	util.AssertNotError(err, "NewWritableFile", t)
+	util.AssertNotError(writableFile.Append([]byte("hello ")), "Append", t)
+	util.AssertNotError(writableFile.Append([]byte("world")), "Append", t)
+	writableFile.Finalize()
+
+	seqFile, err := test.env.NewSequentialFile("/dir/f")
+	util.AssertNotError(err, "NewSequentialFile", t)
+	b := make([]byte, 5)
+	result, _, err := seqFile.Read(b)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, bytes.Compare(result, []byte("hello")), "Compare", t)
+	util.AssertNotError(seqFile.Skip(1), "Skip", t)
+	b = make([]byte, 1000)
+	result, _, err = seqFile.Read(b)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, bytes.Compare(result, []byte("world")), "Compare", t)
+	result, _, err = seqFile.Read(b)
+	util.AssertEqual(0, len(result), "result", t)
+	util.AssertNotError(seqFile.Skip(100), "Skip", t)
+	result, _, err = seqFile.Read(b)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, len(result), "result", t)
+	seqFile.Finalize()
+
+	randFile, err := test.env.NewRandomAccessFile("/dir/f")
+	util.AssertNotError(err, "NewRandomAccessFile", t)
+	b = make([]byte, 5)
+	result, _, err = randFile.Read(b, 6)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, bytes.Compare(result, []byte("world")), "Compare", t)
+	result, _, err = randFile.Read(b, 0)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, bytes.Compare(result, []byte("hello")), "Compare", t)
+	b = make([]byte, 100)
+	result, _, err = randFile.Read(b, 10)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, bytes.Compare(result, []byte("d")), "Compare", t)
+
+	b = make([]byte, 5)
+	_, _, err = randFile.Read(b, 1000)
+	util.AssertError(err, "Read", t)
+	randFile.Finalize()
+}
+
+func TestLocks(t *testing.T) {
+	test := newMemEnvTest()
+	lock, err := test.env.LockFile("some file")
+	util.AssertNotError(err, "LockFile", t)
+	err = test.env.UnlockFile(lock)
+	util.AssertNotError(err, "UnlockFile", t)
+}
+
+func TestMisc(t *testing.T) {
+	test := newMemEnvTest()
+	testDir, err := test.env.GetTestDirectory()
+	util.AssertNotError(err, "GetTestDirectory", t)
+	util.AssertTrue(len(testDir) != 0, "testDir", t)
+
+	writableFile, err := test.env.NewWritableFile("/a/b")
+	util.AssertNotError(err, "NewWritableFile", t)
+
+	util.AssertNotError(writableFile.Sync(), "Sync", t)
+	util.AssertNotError(writableFile.Flush(), "Flush", t)
+	util.AssertNotError(writableFile.Close(), "Close", t)
+	writableFile.Finalize()
+}
+
+func TestLargeWrite(t *testing.T) {
+	const writeSize = 300 * 1024
+	test := newMemEnvTest()
+
+	buf := bytes.NewBufferString("")
+	for i := 0; i < writeSize; i++ {
+		buf.WriteByte(byte(i))
+	}
+	writeData := buf.Bytes()
+
+	writableFile, err := test.env.NewWritableFile("/dir/f")
+	util.AssertNotError(err, "NewWritableFile", t)
+	util.AssertNotError(writableFile.Append([]byte("foo")), "Append", t)
+	util.AssertNotError(writableFile.Append(writeData), "Append", t)
+	writableFile.Finalize()
+
+	seqFile, err := test.env.NewSequentialFile("/dir/f")
+	util.AssertNotError(err, "NewSequentialFile", t)
+	b := make([]byte, 3)
+	result, _, err := seqFile.Read(b)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, bytes.Compare(result, []byte("foo")), "Compare", t)
+
+	read := 0
+	readData := make([]byte, 0, len(writeData))
+	b = make([]byte, writeSize)
+	for read < writeSize {
+		result, _, err = seqFile.Read(b)
+		readData = append(readData, result...)
+		read += len(result)
+	}
+	util.AssertEqual(readData, writeData, "read write data", t)
+	seqFile.Finalize()
+}
+
+func TestOverwriteOpenFile(t *testing.T) {
+	test := newMemEnvTest()
+
+	write1Data := "Write #1 data"
+	fileDataLen := len(write1Data)
+	testFileName := tmpDir() + "/leveldb-TestFile.dat"
+
+	err := ssdb.WriteBytesToFile(test.env, []byte(write1Data), testFileName)
+	util.AssertNotError(err, "WriteBytesToFile", t)
+
+	randFile, err := test.env.NewRandomAccessFile(testFileName)
+	util.AssertNotError(err, "NewRandomAccessFile", t)
+
+	write2Data := "Write #2 data"
+	err = ssdb.WriteBytesToFile(test.env, []byte(write2Data), testFileName)
+	util.AssertNotError(err, "WriteBytesToFile", t)
+
+	b := make([]byte, fileDataLen)
+	result, _, err := randFile.Read(b, 0)
+	util.AssertNotError(err, "Read", t)
+	util.AssertEqual(0, bytes.Compare(result, []byte(write2Data)), "Compare", t)
+
+	randFile.Finalize()
+}
+
+func TestDBTest(t *testing.T) {
+	test := newMemEnvTest()
+	options := ssdb.NewOptions()
+	options.CreateIfMissing = true
+	options.Env = test.env
+
+	keys := []string{"aaa", "bbb", "ccc"}
+	vals := []string{"foo", "bar", "baz"}
+
+	d, err := Open(options, "/dir/db")
+	util.AssertNotError(err, "Open", t)
+	for i := 0; i < 3; i++ {
+		err = d.Put(ssdb.NewWriteOptions(), []byte(keys[i]), []byte(vals[i]))
+		util.AssertNotError(err, "Put", t)
+	}
+
+	for i := 0; i < 3; i++ {
+		res, err := d.Get(ssdb.NewReadOptions(), []byte(keys[i]))
+		util.AssertNotError(err, "Get", t)
+		util.AssertEqual(string(res), vals[i], "value", t)
+	}
+
+	iterator := d.NewIterator(ssdb.NewReadOptions())
+	iterator.SeekToFirst()
+	for i := 0; i < 3; i++ {
+		util.AssertTrue(iterator.Valid(), "Valid", t)
+		util.AssertEqual([]byte(keys[i]), iterator.Key(), "Key", t)
+		util.AssertEqual([]byte(vals[i]), iterator.Value(), "Value", t)
+		iterator.Next()
+	}
+	util.AssertFalse(iterator.Valid(), "Valid", t)
+	iterator.Finalize()
+
+	dbi := d.(*db)
+	util.AssertNotError(dbi.testCompactMemTable(), "testCompactMemTable", t)
+
+	for i := 0; i < 3; i++ {
+		res, err := d.Get(ssdb.NewReadOptions(), []byte(keys[i]))
+		util.AssertNotError(err, "Get", t)
+		util.AssertEqual(string(res), vals[i], "value", t)
+	}
+	dbi.finalize()
+}
