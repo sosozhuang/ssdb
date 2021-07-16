4368ab54e1350ae196b62b4cdf27e437f80034a0
sosozhuang <sosozhuang@163.com>
2021-07-16 23:54:11 +0800

    Fix: remaining bytes offset

--- util/hash.go
+++ util/hash.go
@@ -7,21 +7,22 @@ func Hash(data []byte, seed uint32) uint32 {
 	limit := len(data)
 	h := seed ^ (uint32(limit) * m)
 	var w uint32
-	for ; start+4 <= limit; start += 4 {
+	for start+4 <= limit {
 		w = DecodeFixed32(data[start:])
+		start += 4
 		h += w
 		h *= m
 		h ^= h >> 16
 	}
 	switch limit - start {
 	case 3:
-		h += uint32(data[2]) << 16
+		h += uint32(data[start+2]) << 16
 		fallthrough
 	case 2:
-		h += uint32(data[1]) << 8
+		h += uint32(data[start+1]) << 8
 		fallthrough
 	case 1:
-		h += uint32(data[0])
+		h += uint32(data[start])
 		h *= m
 		h ^= h >> r
 		break
